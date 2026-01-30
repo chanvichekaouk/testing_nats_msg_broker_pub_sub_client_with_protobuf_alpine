69a881a452fb993284daaff32354ca502c22d5ae
Chanvicheka Ouk <Chanvicheka.Ouk@gmail.com>
2026-01-30 19:29:51 +0000

    commit protobuf compiled telemetry.proto related .h & .cpp file. And NatsClient inteface

---
+++ include/nats_client.hpp
@@ -0,0 +1,17 @@
+// nats_client.hpp
+#program once
+#include <string>
+#include <functional>
+
+// pure abstract class
+class NatsClient {
+public:
+	using callback = std::function<void(const std::string&)>; // this is just like func<> delegate in c#
+
+	virtual ~NatClient() = default;
+	virtual bool connect(const std::string& url) = 0;
+	virtual void disconnect() = 0;
+	virtual bool isConnected() const = 0;
+	virtual bool publish(const std::string& subject, const std::string& message) =	 0;
+	virtual bool subscribe(const std::string& subject, callback cb) = 0;			
+};
